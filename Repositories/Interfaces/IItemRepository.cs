f3631193eeca60fc63adb27b9948dd0f4a1752cc
laevad <77134175+laevad@users.noreply.github.com>
2023-11-01 06:34:01 +0800

    repository

---
+++ Repositories/Interfaces/IItemRepository.cs
@@ -0,0 +1,45 @@
+﻿using Play.Catalog.Entities;
+
+namespace Play.Catalog.Repositories.Interfaces;
+
+public interface IItemRepository
+{
+    /// <summary>
+    /// Retrieves all items asynchronously.
+    /// </summary>
+    /// <returns>An enumerable collection of items.</returns>
+    Task<IEnumerable<Item>> GetAsync();
+    
+    
+    /// <summary>
+    /// Creates a new item asynchronously.
+    /// </summary>
+    /// <param name="item">The item to create.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task CreateAsync(Item item);
+    
+    // read
+    /// <summary>
+    /// Retrieves an item with the specified ID from the repository.
+    /// </summary>
+    /// <param name="id">The ID of the item to retrieve.</param>
+    /// <returns>The retrieved item, or null if no item with the specified ID was found.</returns>
+    Task<Item?> GetAsync(Guid id);
+    
+    // update
+    /// <summary>
+    /// Updates an existing item in the repository.
+    /// </summary>
+    /// <param name="id">The ID of the item to update.</param>
+    /// <param name="item">The updated item.</param>
+    /// <returns>A boolean indicating whether the update was successful.</returns>
+    Task<bool> UpdateAsync(Guid id, Item item);
+    
+    // delete
+    /// <summary>
+    /// Deletes an item with the specified ID from the repository.
+    /// </summary>
+    /// <param name="id">The ID of the item to delete.</param>
+    /// <returns>A task that represents the asynchronous delete operation. The task result contains a boolean value indicating whether the delete operation was successful.</returns>
+    Task<bool> DeleteAsync(Guid id);
+}
---
+++ Repositories/ItemRepository.cs
@@ -0,0 +1,71 @@
+﻿using MongoDB.Driver;
+using Play.Catalog.Data.Interfaces;
+using Play.Catalog.Entities;
+using Play.Catalog.Repositories.Interfaces;
+
+namespace Play.Catalog.Repositories;
+
+/// <summary>
+/// Repository class for managing items.
+/// </summary>
+public class ItemRepository : IItemRepository
+{
+    private readonly IItemContext _context;
+
+    public ItemRepository(IItemContext context)
+    {
+        this._context = context;
+    }
+
+    /// <summary>
+    /// Retrieves all items from the database.
+    /// </summary>
+    /// <returns>An enumerable collection of Item objects.</returns>
+    public async Task<IEnumerable<Item>> GetAsync()
+    {
+        return await _context.Items.Find(item => true).ToListAsync();
+    }
+
+    /// <summary>
+    /// Creates a new item asynchronously.
+    /// </summary>
+    /// <param name="item">The item to create.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task CreateAsync(Item item)
+    {
+        await _context.Items.InsertOneAsync(item);
+    }
+
+    /// <summary>
+    /// Retrieves an item from the database by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the item to retrieve.</param>
+    /// <returns>The retrieved item, or null if it doesn't exist.</returns>
+    public async Task<Item?> GetAsync(Guid id)
+    {
+        return await _context.Items.Find(item => item.Id == id).FirstOrDefaultAsync();
+    }
+
+    /// <summary>
+    /// Updates an item in the database.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="item">The item to update.</param>
+    /// <returns>A boolean indicating whether the update was successful.</returns>
+    public async Task<bool> UpdateAsync(Guid id, Item item)
+    {
+        var updateResult = await _context.Items.ReplaceOneAsync(filter: item1 => item1.Id == id, replacement: item);
+        return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+    }
+
+    /// <summary>
+    /// Deletes an item from the database.
+    /// </summary>
+    /// <param name="id">The ID of the item to delete.</param>
+    /// <returns>A boolean indicating whether the deletion was successful.</returns>
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var deleteResult = await _context.Items.DeleteOneAsync(item => item.Id == id);
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
+}
