c02f99304fa2092eb26c4c34bcd1592d4768eb09
laevad <77134175+laevad@users.noreply.github.com>
2023-10-31 23:45:30 +0800

    required and range

--- DTOs/Dtos.cs
+++ DTOs/Dtos.cs
@@ -1,4 +1,6 @@
-﻿namespace Play.Catalog.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Play.Catalog.DTOs;
 
 public class Dtos
 {
@@ -10,10 +12,10 @@ public class Dtos
     /// <summary>
     /// Represents the data transfer object for creating an item.
     /// </summary>
-    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record CreateItemDto([Required] string Name, string Description, [Range(0,1000)]  decimal Price);
 
     /// <summary>
     /// Represents the data transfer object for updating an item.
     /// </summary>
-    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0,1000)] decimal Price);
 }
