e30b5e5dee26034adeaf2dbb1750571dce483ea8
laevad <77134175+laevad@users.noreply.github.com>
2023-11-01 06:33:05 +0800

    data

---
+++ Data/Contexts/ItemContext.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using Play.Catalog.Data.Interfaces;
+using Play.Catalog.Data.Seeds;
+using Play.Catalog.Entities;
+
+namespace Play.Catalog.Data.Contexts;
+
+/// <summary>
+/// Represents the MongoDB context for the Item entity.
+/// </summary>
+public class ItemContext : IItemContext
+{
+    public ItemContext()
+    {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("Catalog");
+        
+        Items = database.GetCollection<Item>("Items");
+        
+        ItemContextSeed.SeedData(Items);
+    }
+    
+
+
+    /// <summary>
+    /// Represents a MongoDB collection of items.
+    /// </summary>
+    public IMongoCollection<Item> Items { get; }
+}
---
+++ Data/Seeds/ItemContextSeed.cs
@@ -0,0 +1,62 @@
+﻿using MongoDB.Driver;
+using Play.Catalog.Entities;
+
+namespace Play.Catalog.Data.Seeds;
+
+/// <summary>
+/// Class for seeding the item collection in the database with preconfigured items.
+/// </summary>
+public class ItemContextSeed
+{
+  /// <summary>
+  /// Seeds the item collection with preconfigured items if it is empty.
+  /// </summary>
+  /// <param name="itemCollection">The MongoDB collection of items.</param>
+  public static void SeedData(IMongoCollection<Item> itemCollection)
+  {
+    var existItem = itemCollection.Find(p => true).Any();
+    if (!existItem)
+    {
+      itemCollection.InsertManyAsync(GetPreconfiguredItems());
+    }
+  }
+
+  /// <summary>
+  /// Returns a list of preconfigured items.
+  /// </summary>
+  /// <returns>A list of preconfigured items.</returns>
+  private static IEnumerable<Item> GetPreconfiguredItems()
+  {
+    return new List<Item>
+    {
+      new()
+      {
+        Name = "Potion",
+        Description = "Restore a small amount of HP.",
+        Price = 5,
+        CreatedDate = DateTimeOffset.UtcNow.Date
+      },
+      new()
+      {
+        Name = "Antidote",
+        Description = "Cures poison.",
+        Price = 7,
+        CreatedDate = DateTimeOffset.UtcNow.Date
+      },
+      new()
+      {
+        Name = "Bronze Sword",
+        Description = "Deals a small amount of damage.",
+        Price = 10,
+        CreatedDate = DateTimeOffset.UtcNow.Date
+      },
+      new()
+      {
+        Name = "Bronze Shield",
+        Description = "Provides a small amount of protection.",
+        Price = 10,
+        CreatedDate = DateTimeOffset.UtcNow.Date
+      }
+    };
+  }
+}
