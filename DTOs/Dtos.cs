a2b2a1724d48c52dea5990740b0949e21cee0647
laevad <77134175+laevad@users.noreply.github.com>
2023-10-31 23:19:54 +0800

    DTO

---
+++ DTOs/Dtos.cs
@@ -0,0 +1,19 @@
+﻿namespace Play.Catalog.DTOs;
+
+public class Dtos
+{
+    /// <summary>
+    /// Represents the data transfer object for an item.
+    /// </summary>
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+    /// <summary>
+    /// Represents the data transfer object for creating an item.
+    /// </summary>
+    public record CreateItemDto(string Name, string Description, decimal Price);
+
+    /// <summary>
+    /// Represents the data transfer object for updating an item.
+    /// </summary>
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
