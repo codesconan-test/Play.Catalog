241c71d09712bdbd066b6c259551567766f315ce
laevad <77134175+laevad@users.noreply.github.com>
2023-10-31 23:48:43 +0800

    added item repository and added mongo driver

---
+++ Entities/Item.cs
@@ -0,0 +1,29 @@
+﻿namespace Play.Catalog.Entities;
+
+public class Item
+{
+    /// <summary>
+    /// The item's unique identifier.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The item's name.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The item's description.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The item's price.
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// The item's created date.
+    /// </summary>
+    public DateTimeOffset CreatedDate { get; set; }
+}
